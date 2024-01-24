f21a213e8a544f94946a6411b77453febfbbadb3
ssnioka <simonas.snioka@gmail.com>
2024-01-24 21:01:56 +0200

    Remove english lanugage

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,7 @@
 module Consul
   class Application < Rails::Application
     config.time_zone = "Vilnius"
-    config.i18n.available_locales = [:lt, :en]
+    config.i18n.available_locales = [:lt]
     config.i18n.default_locale = :lt
 
     config.action_dispatch.default_headers = {
