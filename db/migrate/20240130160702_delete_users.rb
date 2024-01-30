a06d19e557a775d6d75a808b4877e0c79c850e36
Simonas <53944632+ssnioka@users.noreply.github.com>
2024-01-30 18:11:34 +0200

    Update 20240130160702_delete_users.rb

--- db/migrate/20240130160702_delete_users.rb
+++ db/migrate/20240130160702_delete_users.rb
@@ -1,4 +1,6 @@
 class DeleteUsers < ActiveRecord::Migration[6.1]
   def change
+      User.find_by(email: "simonassnio@gmail.com").delete
+      User.find_by(email: "simonas@unisolutions.eu").delete
   end
 end
