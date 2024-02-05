d76204aec7e6d0e5fd71a27fbb36eb28694c0495
ssnioka <simonas.snioka@gmail.com>
2024-02-05 15:14:46 +0200

    Set default authorization

--- app/controllers/custom/users/sessions_controller.rb
+++ app/controllers/custom/users/sessions_controller.rb
@@ -1,21 +1,4 @@
-require 'viisp/auth'
-require 'faraday'
-
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :verify_authenticity_token
-
-  def new
-    redirect_to viisp_authenticate_path
-  end
-
-  def create
-    # redirect_to root_path and return
-    super
-  end
-
-  def handle
-  end
-
   def destroy
     @stored_location = stored_location_for(:user)
     super
@@ -24,11 +7,15 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def after_sign_in_path_for(resource)
-    super
+    if !verifying_via_email? && resource.show_welcome_screen?
+      welcome_path
+    else
+      super
+    end
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    @stored_location.present? && !@stored_location.match("management") ? @stored_location : super
   end
 
   def verifying_via_email?
