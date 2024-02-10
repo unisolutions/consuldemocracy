9ee1425e8a74e7ab338c2f4ad3047bd891445aa8
ssnioka <simonas.snioka@gmail.com>
2024-02-10 22:30:58 +0200

    modify user.rb

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -82,7 +82,7 @@ class User < ApplicationRecord
   belongs_to :geozone
 
   validates :username, presence: true, if: :username_required?
-  validates :phone_number, numericality: { only_integer: true }
+  validates :phone_number, numericality: { allow_blank: true }
   validates :username, uniqueness: { scope: :registering_with_oauth }, if: :username_required?
   validates :document_number, uniqueness: { scope: :document_type }, allow_nil: true
 
