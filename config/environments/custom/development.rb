88d7f83db4ca3771e8df7f3b93f688e382db8ba2
ssnioka <simonas.snioka@gmail.com>
2024-02-13 15:58:57 +0200

    Edit anon user credentials

--- config/environments/custom/development.rb
+++ config/environments/custom/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Overwrite settings for the development environment or add your own
   # custom settings for this environment.
-  config.anonymous_credentials = 'XXXXXXXXXXXXXX'
+  config.anonymous_credentials = '123456789123456'
 
 end
