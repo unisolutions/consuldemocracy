3869d28b3900515f161f1f51fc76a947633385d3
ssnioka <simonas.snioka@gmail.com>
2024-02-13 13:38:22 +0200

    Create anon for prod

--- config/environments/custom/production.rb
+++ config/environments/custom/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Overwrite settings for the production environment or add your own
   # custom settings for this environment.
   #Anonymous data
-  config.anonymous_credentials = 'X1X2X3X4X5X6X78X'
+  config.anonymous_credentials = 'XXXXXXXXXXXXXXX'
 
   #VIISP credentials
   config.viisp_pid = 'VSID000000005850'
