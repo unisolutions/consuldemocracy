d8384a795aa2886db192d7a67d0bbf841d866295
ssnioka <simonas.snioka@gmail.com>
2024-03-26 17:36:03 +0200

    Update production config

--- config/environments/custom/production.rb
+++ config/environments/custom/production.rb
@@ -2,14 +2,23 @@ Rails.application.configure do
   # Overwrite settings for the production environment or add your own
   # custom settings for this environment.
   #Anonymous data
+  # config.anonymous_credentials = '123456789123456'
+  #
+  # #VIISP credentials
+  # config.viisp_pid = 'VSID000000005850'
+  # config.viisp_key_route = './config/keys/dalyvauk-private.pem'
+  # config.base_url = 'https://dalyvauk.krs.lt'
+  # config.viisp_test = false
+  #
+  # config.krs_endpoint = 'http://192.168.201.90/wp-json/krsapi/v1'
+
   config.anonymous_credentials = '123456789123456'
 
-  #VIISP credentials
-  config.viisp_pid = 'VSID000000005850'
-  config.viisp_key_route = './config/keys/dalyvauk-private.pem'
-  config.base_url = 'https://dalyvauk.krs.lt'
-  config.viisp_test = false
+  config.viisp_pid = 'VSID000000000113'
+  config.viisp_key_route = './config/keys/testKey.pem'
+  config.base_url = 'http://80.208.231.133'
+  config.viisp_test = true
 
-  config.krs_endpoint = 'http://192.168.201.90/wp-json/krsapi/v1'
+  config.krs_endpoint = 'http://195.182.94.66:8888/wp-json/krsapi/v1'
 
 end
