3966e901755bf83ffee9e77b237fae7910a90fa9
ssnioka <simonas.snioka@gmail.com>
2024-04-15 01:08:41 +0300

    Fix case, when navigating to results page cause crash

--- app/controllers/custom/budgets/results_controller.rb
+++ app/controllers/custom/budgets/results_controller.rb
@@ -8,10 +8,12 @@ module Budgets
 
     def show
       authorize! :read_results, @budget
-      if params[:heading_id] != nil
+      if params[:heading_id].present?
         @investments = Budget::Result.new(@budget, @heading, nil).investments
-      end
-      if params[:group_id] != nil
+      elsif params[:group_id].present?
+        @investments = Budget::Result.new(@budget, nil, @group).investments
+        @total_votes = total_votes_in_group(@group)
+      else
         @investments = Budget::Result.new(@budget, nil, @group).investments
         @total_votes = total_votes_in_group(@group)
       end
@@ -22,31 +24,32 @@ module Budgets
 
     private
 
-      def load_budget
-        @budget = Budget.find_by_slug_or_id(params[:budget_id]) || Budget.first
-      end
+    def load_budget
+      @budget = Budget.find_by_slug_or_id(params[:budget_id]) || Budget.first
+    end
 
-      def load_heading
-        if @budget.present?
-          headings = @budget.headings
-          @heading = headings.find_by_slug_or_id(params[:heading_id]) || headings.first
-        end
+    def load_heading
+      if @budget.present?
+        headings = @budget.headings
+        @heading = headings.find_by_slug_or_id(params[:heading_id]) || headings.first
       end
+    end
 
-      def load_groups
-        if @budget.present?
-          groups = @budget.groups
-          @group = groups.find_by_slug_or_id(params[:group_id]) || groups.first
+    def load_groups
+      if @budget.present?
+        groups = @budget.groups
+        @group = groups.find_by_slug_or_id(params[:group_id]) || groups.first
+      end
+    end
+
+    def total_votes_in_group(group)
+      total_votes = 0
+      group.headings.each do |heading|
+        heading.investments.selected.each do |investment|
+          total_votes += investment.ballot_lines_count
         end
       end
-      def total_votes_in_group(group)
-        total_votes = 0
-        group.headings.each do |heading|
-          heading.investments.selected.each do |investment|
-            total_votes += investment.ballot_lines_count
-          end
-        end
-        total_votes
-      end
+      total_votes
+    end
   end
 end
